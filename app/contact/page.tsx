0c91524c980a2344301eecf01d06ae46ef2fc83d
Miss Web Dev <mswebdev175@gmail.com>
2024-05-24 23:07:39 +0300

    Image optimisation

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 function Contact() {
   return (
     <section className="text-gray-600 body-font relative">
-      <div className="absolute inset-0 bg-gray-300">
+      <div className="absolute inset-0">
         <iframe
           width="100%"
           height="100%"
           frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
+          marginHeight={0}
+          marginWidth={0}
           title="map"
           scrolling="no"
           src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
